cbae62dedfd5ab75f8abc27b6b38eedfe526488a
naresh50787 <naresh50787@gmail.com>
2021-09-21 11:55:55 +0530

    Create reverseAllWords.php

---
+++ reverseAllWords.php
@@ -0,0 +1,6 @@
+<?php
+/*Write a program to reverse all words but not string.
+Let's assume string is: "I Have got a program to reverse the word but not string."
+We wants to do: "I evaH tog a margorp ot esrever eht drow ton gnirts".
+*/
+?>
